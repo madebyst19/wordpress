77c149ee0f0be52af23a2a5a330471e62d3789d0
sawaki <tomo8409t@gmail.com>
2020-12-27 21:27:49 +0900

    initilize_commit

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php 
+function init_func(){
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+
+    register_post_type('item',[
+        'labels'=> [
+            'name' => '商品',
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'hierarchical' => true,
+        'supports ' => [
+            'title',
+            'editor',
+            'page-attributes'
+        ],
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-cart'
+
+    ]);
+}
+add_action('init','init_func');
