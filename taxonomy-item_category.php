d05275d76d693f1635c8527adcc6073c3b64699e
sawaki <tomo8409t@gmail.com>
2020-12-30 17:43:50 +0900

    done_wordpress_video

--- taxonomy-item_category.php
+++ taxonomy-item_category.php
@@ -2,21 +2,21 @@
 <html lang="ja">
 
 <head>
-
+ 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap core CSS -->
+ <!-- Bootstrap core CSS -->
   <link href="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="<?php echo get_template_directory_uri(); ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
   <link href="<?php echo get_template_directory_uri(); ?>/css/clean-blog.min.css" rel="stylesheet">
-<?php wp_head();?>
+    <?php wp_head(); ?>
 </head>
 
 <body>
@@ -24,7 +24,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <a class="navbar-brand" href="index.html"><?php bloginfo('name'); ?></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -47,48 +47,49 @@
       </div>
     </div>
   </nav>
-<?php while(have_posts()): the_post();?>
-  <!-- Page Header -->
-  <?php 
-  $id = get_post_thumbnail_id();
-  $img = wp_get_attachment_image_src($id);
-  ?>
 
-  <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>)">
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/home-bg.jpg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-heading">
-            <?php the_title();?>
+          <div class="site-heading">
+            <h1>カテゴリ別商品一覧あ</h1>
+            <span class="subheading">A Blog Theme by Start Bootstrap</span>
           </div>
         </div>
       </div>
     </div>
   </header>
 
-  <!-- Post Content -->
-  <article>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-            <?php the_content();?>
-            <dl>
-              <dt>カテゴリー</dt>
-              <dt>価格</dt>
-              <dd><?php echo number_format(get_field('価格')); ?>円</dt>
-              <dt>発売日</dt>
-              <dd><?php the_field('発売日');?></dd>
-          </dl>
-              </div>
-            </div>
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+      <?php while(have_posts()):the_post();  ?>
+        <div class="post-preview">
+          <a href="<?php the_permalink();?>">
+            <h2 class="post-title">
+            <?php the_title();?>
+            </h2>
+            <h3 class="post-subtitle">
+              <?php the_excerpt(); ?>
+            </h3>
+          </a>
+        </div>
+        <hr>
+        <?php endwhile ?>
+        <!-- Pager -->
+        <div class="clearfix">
+       <?php echo paginate_links();?>
+        </div>
+      </div>
     </div>
-  </article>
- 
-        
+  </div>
 
   <hr>
-<?php endwhile ;?>
+
   <!-- Footer -->
   <footer>
     <div class="container">
@@ -132,7 +133,7 @@
 
   <!-- Custom scripts for this template -->
   <script src="<?php echo get_template_directory_uri(); ?>/js/clean-blog.min.js"></script>
-<?php wp_footer();?>
+<?php wp_footer(); ?>
 </body>
 
 </html>
